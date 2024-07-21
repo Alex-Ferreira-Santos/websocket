d005467af55f2dccd83edc742c9bdb18ff1293d5
Alex Ferreira Santos <alexsantos@pecege.com>
2024-07-21 15:33:04 -0300

    feat: atualizando textarea apenas no documento correto

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -2,8 +2,13 @@
 import {atualizaTextoEditor} from './documento.js'
 const socket = io()
 
+
+export function selecionarDocumento(nome){
+  socket.emit("selecionar_documento", nome)
+}
+
 socket.on("texto_editor_clientes", atualizaTextoEditor)
 
-export function emitirTextoEditor(texto){
-  socket.emit("texto_editor", texto)
+export function emitirTextoEditor(dados){
+  socket.emit("texto_editor", dados)
 }
