79ee85b39de9b92ddf095da6715cdf1d51c4eacd
Alex Ferreira Santos <alexsantos@pecege.com>
2024-07-21 14:55:25 -0300

    feat: enviando texto para o back

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,7 @@
 const socket = io()
+
+const textoEditor = document.getElementById('editor-texto')
+
+textoEditor.addEventListener("keyup",(e) => {
+  socket.emit("texto_editor", textoEditor.value)
+})
